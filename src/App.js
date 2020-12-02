a494568b2f98cb213667d5505e493417e153d611
Daniel Becker <dbecker.fl@gmail.com>
2020-12-01 20:57:16 -0500

    Initial Keg View

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Link, Route } from "react-router-dom";
+import KegView from "./views/KegView";
 
 function App() {
     return (
         <BrowserRouter>
-            <Link to={"/view"}>View</Link>
-            <Link to={"/admin"}>Admin</Link>
-            <Route path={"/admin"} component = {Admin} />
-            <Route path={"/view"} component = {View} />
+            <Route path={"/admin"} render={() => {return <p>Admin</p>}} />
+            <Route path={"/view"} component = {KegView} />
         </BrowserRouter>
     )
 
 }
-
-const Admin = () => {
-    return (
-        <p>Admin</p>
-    )
-}
-
-const View = () => {
-    return (
-        <p>View</p>
-    )
-}
-
 
 export default App;

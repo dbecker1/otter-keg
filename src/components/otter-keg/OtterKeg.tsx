e93af4e20cddb88fbf02cce1382905e36ece8bb4
Hanwen Xu <xuhanwen97@gmail.com>
2020-12-05 22:23:58 -0500

    First start of work for board section itself

--- src/components/otter-keg/OtterKeg.tsx
+++ src/components/otter-keg/OtterKeg.tsx
@@ -1,46 +1,19 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { populate, useFirebaseConnect } from 'react-redux-firebase'
-import { OtterKegState } from "../../state/OtterKegState";
-import { Classes } from "@blueprintjs/core";
 import { OtterKegHeader } from "./OtterKegHeader";
 import { OtterKegMain } from "./OtterKegMain";
-import { UserBoard } from "./UserBoard";
+import { Board } from "./board/Board";
 import "../../styles/otter-keg/OtterKeg.scss";
 
 export const OtterKeg = React.memo(function OtterKeg() {
 
-    /*
-    const populates = [
-        { child: 'activeBeers', root: 'beers' },
-    ]
-
-    useFirebaseConnect([
-        // passing populates parameter also creates all necessary child queries
-        { path: 'admin', populates }
-    ]);
-
-    const admin = useSelector((state: OtterKegState) => {return (populate(state.firebase, 'admin', populates))} );
-    
-    
-    const admin = undefined;
-    if (admin === undefined) {
-        return (
-            <div>
-                <h1>Loading...</h1>
-            </div>
-        )
-    }
-
-    const beers = admin;
-    */
     return (
-        <React.Fragment>
+            <React.Fragment>
             <OtterKegHeader />
             <div className="otter-keg-body">
                 <OtterKegMain />
-                <UserBoard />
+                <Board />
             </div>
         </React.Fragment>
+        
     )
 })
---
+++ src/components/otter-keg/board/Board.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { useFirebaseConnect } from "react-redux-firebase";
+import { useSelector } from "react-redux";
+import { OtterKegState } from "../../../state/OtterKegState";
+import { UserBoard } from "./UserBoard";
+
+export const Board = React.memo(function Board() {
+
+    useFirebaseConnect("config");
+    useFirebaseConnect("drinkers");
+
+    const shouldShowLeaderBoard = useSelector((state: OtterKegState) => state.firebase.data.config?.showLeadership ?? {});
+    const leadershipBoardUpdateTime = useSelector((state: OtterKegState) => state.firebase.data.config?.leaderboardDuration ?? 0);
+    const drinkers = useSelector((state: OtterKegState) => state.firebase.data.drinkers);
+
+    // TODO leader board
+    return (
+        shouldShowLeaderBoard ? <UserBoard drinkers={drinkers} /> : <UserBoard drinkers={drinkers} />
+    )
+})

07a833d89a3e03ad86e33a52a8e08cb65b0cedaa
Hanwen Xu <xuhanwen97@gmail.com>
2020-12-05 22:58:20 -0500

    first go at main otter keg

--- src/components/otter-keg/OtterKegMain.tsx
+++ src/components/otter-keg/OtterKegMain.tsx
@@ -1,5 +1,20 @@
 import * as React from "react";
+import { useFirebaseConnect, populate } from "react-redux-firebase";
+import { useSelector } from "react-redux";
+import { OtterKegState } from "../../state/OtterKegState";
+import { Spinner } from "@blueprintjs/core";
+import "../../styles/otter-keg/OtterKegMain.scss";
 
 export const OtterKegMain = React.memo(function OtterKegMain() {
-    return <div>This is main otter keg</div>
+
+    const populates = [{ child: "beerId", root: "beers"}];
+
+    useFirebaseConnect([{path: "kegs", populates, queryParams: [ 'orderByChild=isActive', 'equalTo=true' ]}]);
+    const beers: Object = useSelector((state: OtterKegState) => populate(state.firebase, "kegs", populates));
+    console.log(beers);
+    return beers != null ? <div className={"otter-keg-main"}>
+        <div className={"beers"}>
+        {Object.entries(beers).map(([id, beer]) => <div key={id}>{beer.beerId.name}</div>)}
+        </div>
+    </div> : <Spinner />
 })

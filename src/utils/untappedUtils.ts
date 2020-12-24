d3a5ac4dc199df653d003cc985ca7deff0ee87d4
Daniel Becker <dbecker.fl@gmail.com>
2020-12-23 21:48:53 -0600

    Added expiring caching

--- src/utils/untappedUtils.ts
+++ src/utils/untappedUtils.ts
@@ -4,7 +4,7 @@ const client_id = "C8305503E9B12A5EBC3829BDEF8C547A2572EF2D"
 const client_secret = "162B6383E2AB7C1635D4D3E90C348A956B3F2F64"
 
 function get(path: String): Promise<any> {
-    
+    console.log("Calling Untappd API: ", path);
     let url = base_url + path
     if (path.indexOf("?") <  0) {
         url = url + "?"
@@ -16,7 +16,7 @@ function get(path: String): Promise<any> {
     return fetch(url).then(response => {
         if (!response.ok) {
             response.text().then(text => {
-                console.log("Request error. Text: ", text)
+                console.log("Request error. Response body: ", text)
             })
             throw new Error(response.statusText)
         }
@@ -24,19 +24,34 @@ function get(path: String): Promise<any> {
     })
 }
 
-export function getBeerDetails(bid: String): Promise<any> {
-    return new Promise((resolve, reject) => {
-        let key = "/beer/info/" + bid
-        let cachedResponse = localStorage.getItem(key);
-        if (!!cachedResponse) {
-            console.log("Fetching Untappd API request from cache:  ", key);
-            resolve(JSON.parse(cachedResponse));
-        } else {
-            console.log("Calling Untappd API: ", key);
-            get(key).then(response => {
-                localStorage.setItem(key, JSON.stringify(response));
-                resolve(response);
-            })
+function getAndSave(path: string): Promise<any> {
+    return get(path).then(response => {
+        let cachedData = {
+            data: response,
+            expiration: new  Date()
         }
+        cachedData.expiration.setDate(cachedData.expiration.getDate() + 1);
+        localStorage.setItem(path, JSON.stringify(cachedData));
+        return Promise.resolve(response);
     })
+}
+
+function getCached(path: string): Promise<any> {
+    let rawCachedResponse = localStorage.getItem(path);
+    if (!rawCachedResponse) {
+        return getAndSave(path);
+    } else {
+        let cachedResponse = JSON.parse(rawCachedResponse);
+        if (new Date(cachedResponse.expiration) < new Date()) {
+            console.log("Expired API response in cache for path: ", path)
+            return getAndSave(path);
+        } else {
+            console.log("Fetching Untappd API request from cache: ", path);
+            return Promise.resolve(cachedResponse.data);
+        }
+    }
+}
+
+export function getBeerDetails(bid: String): Promise<any> {
+    return getCached("/beer/info/" + bid);
 }

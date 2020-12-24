07ac8ad8072710991aac5373605ee55617f512db
Daniel Becker <dbecker.fl@gmail.com>
2020-12-23 22:52:30 -0600

    restructure keg loading

--- src/components/otter-keg/OtterKegMain.tsx
+++ src/components/otter-keg/OtterKegMain.tsx
@@ -7,8 +7,13 @@ import { getBeerDetails } from "../../utils/untappedUtils";
 import "../../styles/otter-keg/OtterKegMain.scss";
 import useDeepCompareEffect from 'use-deep-compare-effect'
 
+const positionValues: any = {
+    "left": 1,
+    "right":  2
+}
+
 export const OtterKegMain = React.memo(function OtterKegMain() {
-    const [beerDetails, setBeerDetails] = React.useState(null);
+    const [kegsWithDetails, setKegsWithDetails] = React.useState<any []>([]);
     const populates = [{ child: "beerId", root: "beers"}];
 
     useFirebaseConnect([{path: "kegs", populates, queryParams: [ 'orderByChild=isActive', 'equalTo=true' ]}]);
@@ -17,25 +22,34 @@ export const OtterKegMain = React.memo(function OtterKegMain() {
 
     useDeepCompareEffect(() => {
         let promises = Object.entries(kegs).map(([id, keg]) => getBeerDetails(keg.beerId.untappedBid))
-        Promise.all(promises).then(details => {
-            let newBeerDetails: any = {}
-            details.forEach((beerResponse: any) => {
-                newBeerDetails[beerResponse.response.beer.bid] = beerResponse.response.beer
+        Promise.all(promises).then(beerDetails => {
+            let newKegsWithDetails: any[] = []
+            Object.keys(kegs).forEach((kegId: string) => {
+                // maddie typescript made me do this i hate typescript why did you make me use it ugh
+                let dumbTypescriptKegs: any = kegs;
+                let keg: any = dumbTypescriptKegs[kegId];
+                let kegWithDetails = {
+                    "position": keg.position,
+                    "kegId": kegId,
+                    "beer": beerDetails.filter((beer: any) => {return beer.response.beer.bid === keg.beerId.untappedBid})[0].response.beer
+                };
+                newKegsWithDetails.push(kegWithDetails);
             });
-            setBeerDetails(newBeerDetails);
+            newKegsWithDetails.sort((a: any, b: any) => {
+                let aValue = positionValues[a.position] || 3;
+                let bValue = positionValues[b.position] || 3;
+                return aValue - bValue;
+            })
+            setKegsWithDetails(newKegsWithDetails);
         }).catch(e => {
             console.log("Error loading beer details", e)
         })
     }, [kegs])
-    console.log("Beer Details: ", beerDetails)
-    return kegs !== null && beerDetails !== null ? <div className={"otter-keg-main"}>
+    console.log("Kegs With Details: ", kegsWithDetails)
+    return kegsWithDetails.length > 0 ? <div className={"otter-keg-main"}>
         <div className={"beers"}>
-        {Object.entries(kegs).map(([id, beer]) => {
-            let details: any = beerDetails![beer.beerId.untappedBid]
-            if (details === undefined) {
-                return null;
-            }
-            return <div key={id}>{details.beer_name}</div>
+        {kegsWithDetails.map((keg, index) => {
+            return <div key={index}>{keg.beer.beer_name}</div>
         })}
         </div>
     </div> : <Spinner />

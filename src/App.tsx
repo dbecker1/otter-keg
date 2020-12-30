eee999d3122766b6fec37388fb89b1858943e423
Daniel Becker <dbecker.fl@gmail.com>
2020-12-29 19:07:00 -0600

    Linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { populate, useFirebaseConnect } from 'react-redux-firebase';
 import { BrowserRouter, Route } from "react-router-dom";
 import useDeepCompareEffect from 'use-deep-compare-effect';

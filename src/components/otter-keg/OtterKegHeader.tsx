b57c745b7dde02b9cb9d75b75f059a54df35bdb1
Daniel Becker <dbecker.fl@gmail.com>
2020-12-29 19:20:16 -0600

    Add Untappd logogit add -A

--- src/components/otter-keg/OtterKegHeader.tsx
+++ src/components/otter-keg/OtterKegHeader.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import "../../styles/otter-keg/OtterKegHeader.scss";
+import pbu from "../../assets/pbu_40_white.png";
 
 export const OtterKegHeader = React.memo(function OtterKegHeader() {
 
-    return (<div className="otter-keg-header">Otter Keg</div>);
+    return (
+        <div className="otter-keg-header">
+            <span>Otter Keg</span>
+            <img src={pbu} alt={"Powered by Untappd"} />
+        </div>
+        );
 })

5e97782b2c03838c56d437bc01154ae7a6d3a755
Daniel Becker <dbecker.fl@gmail.com>
2021-02-07 17:57:32 -0500

    Base Chart

---
+++ src/components/otter-keg/Charts.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { useSelector } from "react-redux";
+import { useFirebaseConnect } from "react-redux-firebase";
+import { OtterKegState } from "../../state/OtterKegState";
+import "../../styles/otter-keg/Charts.scss";
+import {XYPlot, HorizontalGridLines, VerticalGridLines, XAxis, YAxis, LineSeries} from "react-vis";
+import {AutoSizer} from 'react-virtualized';
+// import 'react-virtualized/styles.css';
+
+export const Charts = React.memo(function Charts() {
+    useFirebaseConnect("drinkers");
+    // this probably isn't a good idea ¯\_(ツ)_/¯
+    useFirebaseConnect([{
+        path: "pours", 
+        queryParams: [ 'orderByChild=isCurrent', "equalTo=false" ],
+    }]);
+
+    let drinkersRaw: any = useSelector((state: OtterKegState) => state.firebase.data.drinkers) ?? {};
+    let poursRaw: any = useSelector((state: OtterKegState) => state.firebase.data.pours) ?? {};
+
+    let chartData: any = {}
+    Object.keys(poursRaw).forEach((pourId) => {
+        let pour = poursRaw[pourId];
+        let date = pour["start"].substring(0, 10);
+        if (pour["drinkerId"] in chartData) {
+            if (date in chartData[pour["drinkerId"]]) {
+                chartData[pour["drinkerId"]][date] += pour["amount"]
+            } else {
+                chartData[pour["drinkerId"]][date] = pour["amount"]
+            }
+        } else {
+            chartData[pour["drinkerId"]] = {}
+            chartData[pour["drinkerId"]][date] = pour["amount"]
+        }
+    });
+    console.log(chartData);
+    return  <div className="charts">
+        <AutoSizer>
+            {({height, width}) => (
+                <XYPlot xType="time" width={width * .8} height={height}>
+                    <HorizontalGridLines />
+                    <VerticalGridLines />
+                    <XAxis title="X Axis" />
+                    <YAxis title="Y Axis" />
+                    {Object.keys(chartData).map((drinkerId) => {
+                        return <LineSeries
+                            data={Object.keys(chartData[drinkerId]).map((date) => {
+                                return {
+                                    x: new Date(date).getTime(),
+                                    y: chartData[drinkerId][date]
+                                }
+                            })}
+                        />
+                    })}
+                </XYPlot>
+            )}
+        </AutoSizer>
+    </div>
+});
--- src/components/otter-keg/OtterKegMain.tsx
+++ src/components/otter-keg/OtterKegMain.tsx
@@ -6,36 +6,64 @@ import "../../styles/otter-keg/OtterKegMain.scss";
 import { KegWithDetails } from "../../types/KegWithDetails";
 import { PourOverlayWrapper } from "./PourOverlayWrapper";
 import { KegStats } from "./KegStats";
+import { Charts } from "./Charts";
 
+const NUM_PAGES = 2;
 export const OtterKegMain = React.memo(function OtterKegMain() {
+    let [activePage, setActivePage] = React.useState(0);
     let activeKegs: KegWithDetails[] = useSelector((state: OtterKegState) => state.activeKegs) || {};
 
-    return activeKegs.length > 0 ? <div className={"otter-keg-main"}>
-        <div className={"keg-details"}>
-            <div className="keg-details-row header">
-                {activeKegs.map((keg, index) => {
-                    return <div className="keg-details-col" key={index}>
-                        <p className="beer-title">{keg.beerDetails.beer_name}</p>
-                        <p>{keg.beerDetails.beer_style}<br />{keg.beerDetails.brewery.brewery_name}</p>
-                    </div>
-                })}
-            </div>
-            <div className="keg-details-row image">
-                {activeKegs.map((keg, index) => {
-                    return <div className="keg-details-col" key={index}>
-                        <img className="beer-logo" src={keg.beerDetails.beer_label_hd || keg.beerDetails.beer_label} alt={keg.beerDetails.beer_name}/>
-                    </div>
-                })}
-            </div>
+    function keycheck(e: any) {
+        if (e.keyCode === 37) {
+            let newActivePage = activePage - 1;
+            if (newActivePage < 0) 
+                newActivePage = NUM_PAGES - 1;
+            setActivePage(newActivePage);
+        } else if (e.keyCode === 39) {
+            let newActivePage = activePage + 1;
+            if (newActivePage >= NUM_PAGES) 
+                newActivePage = 0;
+            setActivePage(newActivePage);
+        }
+    }
 
-            <div className="keg-details-row footer">
-                {activeKegs.map((keg, index) => {
-                    return <div className="keg-details-col" key={index}>
-                        <KegStats keg={keg} />
+    React.useEffect(() => {
+        window.addEventListener("keydown", keycheck);
+        return () => {
+            window.removeEventListener("keydown", keycheck)
+        }
+    })
+    return activeKegs.length > 0 ? <div className={"otter-keg-main"}>
+            {activePage === 0 && 
+                <div className={"keg-details"}>
+                    <div className="keg-details-row header">
+                        {activeKegs.map((keg, index) => {
+                            return <div className="keg-details-col" key={index}>
+                                <p className="beer-title">{keg.beerDetails.beer_name}</p>
+                                <p>{keg.beerDetails.beer_style}<br />{keg.beerDetails.brewery.brewery_name}</p>
+                            </div>
+                        })}
                     </div>
-                })}
-            </div>
-        </div>
+                    <div className="keg-details-row image">
+                        {activeKegs.map((keg, index) => {
+                            return <div className="keg-details-col" key={index}>
+                                <img className="beer-logo" src={keg.beerDetails.beer_label_hd || keg.beerDetails.beer_label} alt={keg.beerDetails.beer_name}/>
+                            </div>
+                        })}
+                    </div>
+
+                    <div className="keg-details-row footer">
+                        {activeKegs.map((keg, index) => {
+                            return <div className="keg-details-col" key={index}>
+                                <KegStats keg={keg} />
+                            </div>
+                        })}
+                    </div>
+                </div>
+            }
+            {activePage === 1 && 
+                <Charts />
+            }
         <PourOverlayWrapper />
     </div> : <Spinner />
-})
+});
